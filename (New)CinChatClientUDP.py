5c894a2cfb7a90d7672823faad5ae86bbcec28f6
José <jlsn@cin.ufpe.br>
2024-01-06 17:49:57 -0300

    Adiciona o menu interativo do usuário e flag quando o comando for 'quit'.

--- (New)CinChatClientUDP.py
+++ (New)CinChatClientUDP.py
@@ -1,9 +1,12 @@
 import socket
 import os
 import threading #Por se tratar de um Chat com múltiplos usuários, precisamos importar a biblioteca threading.
-
+import time
 # Create a UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#Variável de controle indicando quando as threads devem ser encerradas
+exit_flag = False
 
 def send_file(filename,HOST,PORT):
     with open(filename, 'rb') as file:
@@ -17,13 +20,19 @@ def send_file(filename,HOST,PORT):
             if not data:
                 break
             client_socket.sendto(data, (HOST, PORT))
+
 def send(name,HOST,PORT):
-	while True:
-		message = input(f"{name} >> ")
-		if message == "quit":
-			os._exit(1)
-		sm = "{} : {}".format(name,message)
-		client_socket.sendto(sm.encode() , (HOST,int(PORT)))
+    global exit_flag 
+
+    while True:
+        if exit_flag:
+            break #sair do loop se a variável indicar a saída
+        message = input(f"{name} >> ")
+        if message == "quit":
+            exit_flag = True #Sinalizando para as threads encerrarem
+            break
+        sm = "{} : {}".format(name, message)
+        client_socket.sendto(sm.encode() , (HOST,int(PORT)))
 
 def received():
 	while True:
@@ -39,9 +48,7 @@ def main():
     # Client configuration
     HOST = str(input('Enter the IP address of the server : '))
     PORT = int(input('Enter the server port : '))
-
     name = input("ENTER YOUR NAME : ")
-    options =str(input('File to be send or quit'))
     
     #Implementação da biblioteca threading para envio e recebimento de mensagens
     send_thread = threading.Thread(target=send, args=(name, HOST, PORT))
@@ -50,7 +57,28 @@ def main():
     #Iniciar threads
     send_thread.start()
     receive_thread.start()
-        
+
+    #Implementação do menu interativo do usuário.
+    while True:
+        option = str(input('Enter "send" to send a file, "wait" to stay in the chat and receive messages, or "quit" to leave the chatroom: '))
+
+        if option.lower() == 'send':
+            pass #Vou implementar o envio de arquivos posteriormente.
+        elif option.lower() == 'wait':
+            pass #Nesse estado o cliente vai ficar somente lendo as mensagens, sem enviar nada, até decidir enviar algo novamente
+        elif option.lower() == 'quit':
+            print("Exiting the program in ", end='', flush = True)
+
+            for i in range(3, 0, -1):
+                 time.sleep(1)
+                 print(f"{i}...", end='', flush = True)
+
+            print("Goodbye! o/")
+            break #Sai do loop e permite a conclusão ordenada das threads.
+        else:
+             print('Invalid option. Please enter "send" to send a file, "wait" to receive messages, or "quit" to exit. ')
+    
+ 
     # Close the socket
     client_socket.close()
 
